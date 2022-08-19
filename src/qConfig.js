21228608928b14c0c32ffdd0830c9fd8a5a52393
kekikcilerakin <kekikcilerakin@gmail.com>
2022-08-19 17:47:10 +0300

    Initial commit

---
+++ src/qConfig.js
@@ -0,0 +1,10 @@
+export default {
+    host: 'desktop-efeakin',
+    secure: true,
+    port: 443,
+    prefix: 'qlikops',
+    xrfkey: "123456789olksgan",
+    isSecure: true,
+    protocol: 'https'
+}
+
---
+++ src/views/Home.vue
@@ -0,0 +1,6 @@
+<template>
+	<main id="home-page">
+		<h1>Home</h1>
+		<p>This is the home page</p>
+	</main>
+</template>
